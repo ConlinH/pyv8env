1d1237d78f8d3283302c9abe0d163299bb5258d3
huangconghua <huangconghua@sics.ac.cn>
2025-05-23 11:45:20 +0800

    update

--- pyv8env/env/config.py
+++ pyv8env/env/config.py
@@ -2,64 +2,7 @@ import sys
 from functools import wraps
 from loguru import logger
 
-
-class CallbackType:
-    kPy = 0         # 回调Python类方法
-    kJs = 1         # 回调Javascript方法 例如：faker.Window_get_frames
-
-
-class FlagImmutable:
-    instance = 1 << 0
-    prototype = 1 << 1
-
-
-class V8Attribute:
-    kNone = 0
-    ReadOnly = 1 << 0  # ReadOnly, i.e., not writable
-    DontEnum = 1 << 1  # DontEnum, i.e., not enumerable.
-    DontDelete = 1 << 2  # DontDelete, i.e., not configurable
-
-
-class FlagLocation:
-    kInstance = 0
-    kPrototype = 1
-    kInterface = 2
-
-
-class FlagReceiverCheck:
-    kCheck = 0
-    kDoNotCheck = 1
-
-
-class FlagCrossOriginCheck:
-    kCheck = 0
-    kDoNotCheck = 1
-
-
-class FlagExposed:
-    kNo = 0
-    kYes = 1
-
-
-class FlagConstructor:
-    kDontAll = 0
-    kNew = 1 << 0
-    kCall = 1 << 1
-    kAll = kNew | kCall
-
-
-class SideEffectType:
-    kHasSideEffect = 0
-    kHasNoSideEffect = 1
-    kHasSideEffectToReceiver = 2
-
-
-class FlagArrayProto:
-    iterator = 1 << 0
-    entries = 1 << 1
-    keys = 1 << 2
-    values = 1 << 3
-    forEach = 1 << 4
+from pyv8.flag import *
 
 
 class AttributeJSConfig:
